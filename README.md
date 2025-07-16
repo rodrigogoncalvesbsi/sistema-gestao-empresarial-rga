6035316f63efb14ef72e37ef61c10459d8aa5720
Rodrigo <ga.rodrygo@gmail.com>
2025-07-16 10:27:17 -0300

    Primeiro commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# sistema-gestao-empresarial-rga
---
+++ src/main/java/br/com/sistema/SistemaGestaoEmpresarialRgaApplication.java
@@ -0,0 +1,14 @@
+package br.com.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoEmpresarialRgaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SistemaGestaoEmpresarialRgaApplication.class, args);
+	}
+
+}
